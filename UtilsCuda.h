073bfdf8a26f7a9619e945d4377c2b6a6c59d8df
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2019-05-11 21:06:37 +0000

    More work on error checking

--- UtilsCuda.h
+++ UtilsCuda.h
@@ -22,11 +22,20 @@
 #define LOGURU_REPLACE_GLOG 1
 #include <loguru.hpp> //needs to be added after torch.h otherwise loguru stops printing for some reason
 
-inline void cuda_error_check(){
 
-    auto code = cudaGetLastError();
-    if(cudaSuccess != code){
-        fprintf(stderr,"GPU Error: %s at %s:%i \n", cudaGetErrorString(code),  __FILE__, __LINE__ );
-        exit(code);
-    }
+// #define CUD_C(stmt) do { 
+//     stmt;
+//     gpuAssert(#stmt, __FILE__, __LINE__);	\
+// } while (0)
+inline void gpuAssert(const char* stmt, const char* fname, int line)
+{
+   auto code = cudaGetLastError();
+   if (code != cudaSuccess) 
+   {
+      printf("CUDA error %s, at %s:%i - for %s.\n", cudaGetErrorString(code), fname, line, stmt);
+      exit(1);
+   }
 }
+// CUD_C Check Macro. Will terminate the program if a error is detected.
+#define CUDA_CHECK_ERROR() { gpuAssert("none", __FILE__, __LINE__);	}
+
